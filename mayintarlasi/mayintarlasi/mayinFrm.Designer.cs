fc05b0e274e578f836f31e267eeb04fa2c004772
Öğr.Gör.Ömer Özcan <69793480+omerozcann@users.noreply.github.com>
2021-12-27 21:25:51 +0300

    .

---
+++ mayintarlasi/mayintarlasi/mayinFrm.Designer.cs
@@ -0,0 +1,145 @@
+﻿
+namespace mayintarlasi
+{
+    partial class mayinFrm
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.sifirlabtn = new System.Windows.Forms.Button();
+            this.zorrb = new System.Windows.Forms.RadioButton();
+            this.ortarb = new System.Windows.Forms.RadioButton();
+            this.kolayrb = new System.Windows.Forms.RadioButton();
+            this.baslabtn = new System.Windows.Forms.Button();
+            this.oyunalanigrp = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.sifirlabtn);
+            this.groupBox1.Controls.Add(this.zorrb);
+            this.groupBox1.Controls.Add(this.ortarb);
+            this.groupBox1.Controls.Add(this.kolayrb);
+            this.groupBox1.Controls.Add(this.baslabtn);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(131, 297);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kontroller";
+            // 
+            // sifirlabtn
+            // 
+            this.sifirlabtn.Location = new System.Drawing.Point(25, 154);
+            this.sifirlabtn.Name = "sifirlabtn";
+            this.sifirlabtn.Size = new System.Drawing.Size(75, 23);
+            this.sifirlabtn.TabIndex = 4;
+            this.sifirlabtn.Text = "Sıfırla";
+            this.sifirlabtn.UseVisualStyleBackColor = true;
+            this.sifirlabtn.Click += new System.EventHandler(this.sifirlabtn_Click);
+            // 
+            // zorrb
+            // 
+            this.zorrb.AutoSize = true;
+            this.zorrb.Location = new System.Drawing.Point(25, 87);
+            this.zorrb.Name = "zorrb";
+            this.zorrb.Size = new System.Drawing.Size(46, 20);
+            this.zorrb.TabIndex = 3;
+            this.zorrb.TabStop = true;
+            this.zorrb.Text = "Zor";
+            this.zorrb.UseVisualStyleBackColor = true;
+            // 
+            // ortarb
+            // 
+            this.ortarb.AutoSize = true;
+            this.ortarb.Location = new System.Drawing.Point(25, 60);
+            this.ortarb.Name = "ortarb";
+            this.ortarb.Size = new System.Drawing.Size(51, 20);
+            this.ortarb.TabIndex = 2;
+            this.ortarb.TabStop = true;
+            this.ortarb.Text = "Orta";
+            this.ortarb.UseVisualStyleBackColor = true;
+            // 
+            // kolayrb
+            // 
+            this.kolayrb.AutoSize = true;
+            this.kolayrb.Location = new System.Drawing.Point(25, 33);
+            this.kolayrb.Name = "kolayrb";
+            this.kolayrb.Size = new System.Drawing.Size(60, 20);
+            this.kolayrb.TabIndex = 1;
+            this.kolayrb.TabStop = true;
+            this.kolayrb.Text = "Kolay";
+            this.kolayrb.UseVisualStyleBackColor = true;
+            // 
+            // baslabtn
+            // 
+            this.baslabtn.Location = new System.Drawing.Point(25, 125);
+            this.baslabtn.Name = "baslabtn";
+            this.baslabtn.Size = new System.Drawing.Size(75, 23);
+            this.baslabtn.TabIndex = 0;
+            this.baslabtn.Text = "Başla";
+            this.baslabtn.UseVisualStyleBackColor = true;
+            this.baslabtn.Click += new System.EventHandler(this.baslabtn_Click);
+            // 
+            // oyunalanigrp
+            // 
+            this.oyunalanigrp.Location = new System.Drawing.Point(149, 12);
+            this.oyunalanigrp.Name = "oyunalanigrp";
+            this.oyunalanigrp.Size = new System.Drawing.Size(351, 297);
+            this.oyunalanigrp.TabIndex = 1;
+            this.oyunalanigrp.TabStop = false;
+            this.oyunalanigrp.Text = "Oyun Alanı";
+            // 
+            // mayinFrm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 318);
+            this.Controls.Add(this.oyunalanigrp);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "mayinFrm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox oyunalanigrp;
+        private System.Windows.Forms.RadioButton kolayrb;
+        private System.Windows.Forms.Button baslabtn;
+        private System.Windows.Forms.RadioButton zorrb;
+        private System.Windows.Forms.RadioButton ortarb;
+        private System.Windows.Forms.Button sifirlabtn;
+    }
+}
+
---
+++ mayintarlasi/mayintarlasi/mayinFrm.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mayintarlasi
+{
+    public partial class mayinFrm : Form
+    {
+        int[,] mayinlar;
+        int boyut;
+        Button[,] bt;
+        public mayinFrm()
+        {
+            InitializeComponent();
+        }
+
+        private void oyunbtn_Click(object sender, EventArgs e)
+        {
+            Button basilan = (Button)sender;
+            String[] koord = basilan.Name.Split('-');
+            int x = int.Parse(koord[0]);
+            int y = int.Parse(koord[1]);
+            if (mayinlar[x,y]==1)
+            {                
+                MessageBox.Show("mayına bastınız");
+
+                for (int i = 0; i < boyut; i++)
+                {
+                    for (int j = 0; j < boyut; j++)
+                    {
+                        if (mayinlar[i, j] == 1)
+                        {
+                            bt[i, j].Text = "1";                            
+                        }                        
+                        bt[i, j].Enabled = false;                       
+                    }
+                }
+            }
+            else
+            {
+                bt[x, y].Enabled = false;
+                int msayi = 0;
+                if((y+1)!=boyut)
+                {
+                   if(mayinlar[x,y+1]==1)
+                    {
+                        msayi++;
+                    }
+                }
+                if (y != 0)
+                {
+                    if (mayinlar[x, y - 1] == 1)
+                    {
+                        msayi++;
+                    }
+                }
+                if ((x + 1) != boyut)
+                {
+                    if (mayinlar[x+1, y] == 1)
+                    {
+                        msayi++;
+                    }
+                }
+                if (x != 0)
+                {
+                    if (mayinlar[x-1, y] == 1)
+                    {
+                        msayi++;
+                    }
+                }
+                bt[x, y].Text = msayi.ToString();
+            }
+        }
+            private void baslabtn_Click(object sender, EventArgs e)
+        {
+            baslabtn.Enabled = false;
+            oyunalanigrp.Controls.Clear();
+            int rsayi = 0;
+            Random r = new Random();
+            if(kolayrb.Checked)
+            {
+                rsayi = r.Next(8);
+                boyut = 3;
+                mayinlar = new int[boyut, boyut];
+                for (int i = 0; i < 1; i++)
+                {
+                    mayinlar[r.Next(2), r.Next(2)] = 1;
+                }
+                bt = new Button[boyut, boyut];
+                
+                for(int i=0;i<boyut;i++)
+                {
+                    for(int j=0;j<boyut;j++)
+                    {
+                        bt[i, j] = new Button();
+                        bt[i, j].Width = 40;
+                        bt[i, j].Height = 40;
+                        bt[i, j].Left = oyunalanigrp.Width/2-(bt[i, j].Width*boyut/2) + j * bt[i, j].Width;
+                        bt[i, j].Top= oyunalanigrp.Height / 2 - (bt[i, j].Height * boyut / 2) + i * bt[i, j].Height;
+                        bt[i, j].Parent = oyunalanigrp;
+                        bt[i, j].Click += new EventHandler(oyunbtn_Click);
+                        bt[i, j].Name = i + "-" + j;
+                    }
+                }
+            }
+            if (ortarb.Checked)
+            {
+                boyut = 4;                
+                mayinlar = new int[boyut, boyut];
+                for (int i = 0; i < boyut; i++)
+                {
+                    mayinlar[r.Next(3), r.Next(3)] = 1;
+                }                              
+               
+                bt = new Button[boyut, boyut];
+                for (int i = 0; i < boyut; i++)
+                {
+                    for (int j = 0; j < boyut; j++)
+                    {
+                        bt[i, j] = new Button();
+                        bt[i, j].Width = 40;
+                        bt[i, j].Height = 40;
+                        bt[i, j].Left = oyunalanigrp.Width / 2 - (bt[i, j].Width * boyut / 2) + j * bt[i, j].Width;
+                        bt[i, j].Top = oyunalanigrp.Height / 2 - (bt[i, j].Height * boyut / 2) + i * bt[i, j].Height;
+                        bt[i, j].Parent = oyunalanigrp;
+                        bt[i, j].Click += new EventHandler(oyunbtn_Click);
+                        bt[i, j].Name = i + "-" + j;
+                    }
+                }
+            }
+            if (zorrb.Checked)
+            {
+                boyut = 5;
+                bt = new Button[boyut, boyut];
+                mayinlar = new int[boyut, boyut];
+                for (int i = 0; i < 8; i++)
+                {
+                    mayinlar[r.Next(4), r.Next(4)] = 1;
+                }
+                for (int i = 0; i < boyut; i++)
+                {
+                    for (int j = 0; j < boyut; j++)
+                    {
+                        bt[i, j] = new Button();
+                        bt[i, j].Width = 40;
+                        bt[i, j].Height = 40;
+                        bt[i, j].Left = oyunalanigrp.Width / 2 - (bt[i, j].Width * boyut / 2) + j * bt[i, j].Width;
+                        bt[i, j].Top = oyunalanigrp.Height / 2 - (bt[i, j].Height * boyut / 2) + i * bt[i, j].Height;
+                        bt[i, j].Parent = oyunalanigrp;
+                        bt[i, j].Click += new EventHandler(oyunbtn_Click);
+                        bt[i, j].Name = i + "-" + j;
+                    }
+                }
+            }
+        }
+
+        private void sifirlabtn_Click(object sender, EventArgs e)
+        {
+            baslabtn.Enabled = true;
+            oyunalanigrp.Controls.Clear();
+        }
+    }
+}
